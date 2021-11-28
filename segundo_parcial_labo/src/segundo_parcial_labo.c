261d18384ec9cfbfb76668e2836e936881bf953d
Mauricio Gonzalez <gonzalezmauricio1418@gmail.com>
2021-11-28 17:05:20 -0300

    segundo_parcial

---
+++ segundo_parcial_labo/src/segundo_parcial_labo.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : segundo_parcial_labo.c
+ Author      : mauricio gonzalez
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
